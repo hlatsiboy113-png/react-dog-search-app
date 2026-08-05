8c56e3d10661d8ee441afd3983df92d1558d881b
hlatsiboy113-png <hlatsiboy113@gmail.com>
2026-08-05 12:08:20 +0200

    Wrap App component with DogProvider for context management

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { DogProvider } from "./context/DogContext";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <DogProvider>
+            <App />
+        </DogProvider>
+    </React.StrictMode>
