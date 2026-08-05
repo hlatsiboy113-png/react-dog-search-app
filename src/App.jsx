ba6d9890d707d0ab6d1a6dbaee996917d65359a4
hlatsiboy113-png <hlatsiboy113@gmail.com>
2026-08-05 12:19:11 +0200

    Refactor App component structure for improved readability

--- src/App.jsx
+++ src/App.jsx
@@ -4,3 +4,18 @@ import SearchBar from "./components/SearchBar";
 import ResultsHeader from "./components/ResultsHeader";
 import DogGrid from "./components/DogGrid";
 
+function App() {
+    return (
+        <>
+            <Navbar />
+
+            <main>
+                <SearchBar />
+                <ResultsHeader />
+                <DogGrid />
+            </main>
+        </>
+    );
+}
+
+export default App;
